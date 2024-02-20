b5e986aa73cfb051f299cf4d2b50162337a469a7
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-20 08:57:17 +0000

    save  change

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,8 +2,13 @@ const accountID=12344
 let accounntEmail = "sample@gmail.com"
 var accoountpassword="password"
 accountCity= "Jaipur"
+accountState;
 
-accountID= 123
+// accountID= 123
 // console.log(accountID);
+/*
+Prefer not use var because of issue in block scope and functional scope
+*/
 
-console.table([accountID,accounntEmail,accoountpassword,accountCity])
+
+console.table([accountID,accounntEmail,accoountpassword,accountCity,accountState])
