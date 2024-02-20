d58fe81b66502c7bda77070e43857d3dc6b3c58e
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-20 08:52:29 +0000

    variable changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountID=12344
+let accounntEmail = "sample@gmail.com"
+var accoountpassword="password"
+accountCity= "Jaipur"
+
+accountID= 123
+// console.log(accountID);
+
+console.table([accountID,accounntEmail,accoountpassword,accountCity])
