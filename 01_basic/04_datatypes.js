0a16c4d97f219f40af66d3de2e25bed65a54f037
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-20 20:18:16 +0000

    done

---
+++ 01_basic/04_datatypes.js
@@ -0,0 +1,13 @@
+const heroes =["Shaktiman","Nagraj","Doga"];
+
+let myobj = {
+    name:"Abhishek",
+    age:22,
+    address:"Motihari",
+}
+
+const myfunction = function()
+{
+    console.log("Hello Everyone ok")
+}
+console.log(myobj)
