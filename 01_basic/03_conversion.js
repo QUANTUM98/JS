ac19774abea01d72432c6d23f9ae0aa3c8bc3c07
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-20 12:35:05 +0000

    conversions

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,15 @@
+let score = "35";
+
+console.log(typeof(score) );
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+//"33"=>>33
+//"33abc"=>>NAN,Not a Number
+//true=>1
+//""=> false;
+//"abc"=>true;
+let a= 33;
+let b = String(a);
+console.log(typeof b);
