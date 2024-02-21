b74f02e973767946c14ff09c360b401708f959f4
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-21 17:17:38 +0000

    ok

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,15 @@
+const name  ="Abhishek";
+const count =10;
+
+// console.log(`Hello my name is ${name} and my reository count is ${count}`);
+
+const a = new String("Anupam");
+let b = 
+{
+    name:'Aman',
+    age:20,
+}
+console.log(b);
+console.log(a)
+console.log(a[2])
+console.log(a.__proto__);
