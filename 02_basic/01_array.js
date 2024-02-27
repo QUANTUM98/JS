ff082a233c0d5d908530589245f2d7aa026a32b6
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-27 16:03:49 +0000

    d

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -13,6 +13,20 @@ arr.push(5);
 const a = arr1.slice(1,3);//do not modifies the array
 const b = arr1.splice(1,3);//modofies the array 
 
-console.log(a);
-console.log(b);
-console.log(arr1);
+//onsole.log(a);
+//console.log(b);
+//console.log(arr1);
+const marvel =["Ironman","Spiderman","Thor"]
+const dc = ["Flash","Arrow","Superman"]
+
+// marvel.push(dc);
+// console.group(marvel)
+const c = marvel.concat(dc);
+// console.log(c);
+
+const allheroes = [...marvel,...dc];//spread operator is used
+// console.log(allheroes)
+
+const d= [1,2,3,[4,5,6,7],[8,9,[10,11]]]
+const e = d.flat(Infinity);
+console.log(e)
