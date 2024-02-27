a4e9dd0fed2ab63c1fb7e5e6b5e58b8777aedff5
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-02-27 15:45:31 +0000

    done

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,5 +1,18 @@
 const arr= [1,2,3,4,"Aman"];
 
-console.log(arr);
+const arr1 = new Array(5,6,7,8,9,10);
 
-console.log(arr[1])
+// console.log(arr);
+
+// console.log(arr[1])
+
+// Array Methods
+arr.push(5);
+// console.log(arr);
+
+const a = arr1.slice(1,3);//do not modifies the array
+const b = arr1.splice(1,3);//modofies the array 
+
+console.log(a);
+console.log(b);
+console.log(arr1);
