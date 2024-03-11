a688fc0b59be438ff57a1d19ac239d9900dd3dd9
QUANTUM98 <94732187+QUANTUM98@users.noreply.github.com>
2024-03-11 10:53:57 +0000

    kk

--- objects/02.js
+++ objects/02.js
@@ -1,52 +1,52 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// // const tinderUser = new Object()
+// const tinderUser = {}
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser.isLoggedIn = false
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
-        }
-    }
-}
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "hitesh",
+//             lastname: "choudhary"
+//         }
+//     }
+// }
 
 // console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4) //{} here is source and others are target
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
 
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-]
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ]
 
-users[1].email
-// console.log(tinderUser);
+// users[1].email
+// // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
@@ -63,19 +63,20 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course // Object de-structure 
 
 // console.log(courseInstructor);
 console.log(instructor);
 
 // {
+
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// [
+//     {},
+//     {},
+//     {}
+// ]
